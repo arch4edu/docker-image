2a3bf505308d96c49bfa831971c9377f16c08906
Jingbei Li <i@jingbei.li>
2019-06-07 14:48:54 +0800

    updated script

---
+++ calil.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+#python packages.py
+packages=$(grep -v '^#' packages.txt)
+packages=$(echo $packages)
+sed "s/PACKAGES/$packages/" Dockerfile.template | DOCKER_BUILDKIT=1 docker build -f - \
+	--no-cache \
+	--pull \
+	--compress \
+	--squash \
+	-t arch4edu/base:latest .
+
+docker save arch4edu/base | zstd -c -T32 --ultra -21 - > arch4edu.tar.zst
+docker push arch4edu/base:latest
--- packages.py
+++ packages.py
@@ -10,75 +10,74 @@ force['cython'] = 'cython'
 force['et_xmlfile'] = 'python-et-xmlfile'
 force['freetype'] = 'freetype2'
 force['glib'] = 'glib2'
+force['graphite2'] = 'graphite'
 force['importlib_metadata'] = 'python-importlib-metadata'
 force['intel-openmp'] = 'intel-openmp'
 force['ipython'] = 'ipython'
+force['jbig'] = 'jbigkit'
 force['jinja2'] = 'python-jinja'
+force['jpeg'] = 'libjpeg-turbo'
 force['jupyter'] = 'jupyter'
 force['jupyter_console'] = 'jupyter_console'
 force['jupyterlab'] = 'jupyterlab'
 force['jupyterlab_server'] = 'jupyterlab_server'
-force['libgcc-ng'] = 'gcc'
-force['libgfortran-ng'] = 'gcc-fortran'
+force['libgcc-ng'] = 'gcc-libs'
+force['libuuid'] = 'libutil-linux'
 force['lz4-c'] = 'lz4'
 force['mkl'] = 'intel-mkl'
 force['msgpack-python'] = 'python-msgpack'
 force['nbconvert'] = 'jupyter-nbconvert'
 force['nbformat'] = 'jupyter-nbformat'
 force['notebook'] = 'jupyter-notebook'
-force['numpy-base'] = 'python-numpy'
+force['numpy-base'] = 'python-numpy-mkl'
+force['pandoc'] = 'pandoc'
+force['py-lief'] = 'lief'
+force['pyyaml'] = 'python-yaml'
 force['pyqt'] = 'python-pyqt5'
 force['qt'] = 'qt5-base'
 force['ruamel_yaml'] = 'python-ruamel-yaml'
+force['scipy'] = 'python-scipy-mkl'
 force['spyder'] = 'spyder'
 force['widgetsnbextension'] = 'jupyter-widgetsnbextension'
+force['yaml'] = 'libyaml'
 
 ignore = []
-ignore.append('_ipyw_jlab_nb_ext_conf')
-ignore.append('anaconda')
-ignore.append('anaconda-client')
-ignore.append('anaconda-navigator')
-ignore.append('anaconda-project')
-ignore.append('backports')
-ignore.append('backports.os')
-ignore.append('backports.shutil_get_terminal_size')
-ignore.append('conda-build')
-ignore.append('conda-env')
-ignore.append('conda-verify')
-ignore.append('clyent')
-ignore.append('distributed')
-ignore.append('libcurl')
-ignore.append('navigator-updater')
-ignore.append('singledispatch')
-ignore.append('unicodecsv')
+ignore.append('_ipyw_jlab_nb_ext_conf') # anaconda related
+ignore.append('anaconda') # anaconda related
+ignore.append('anaconda-client') # anaconda related
+ignore.append('anaconda-navigator') # anaconda related
+ignore.append('anaconda-project') # anaconda related
+ignore.append('backcall') # already provided by ipython
+ignore.append('backports') # not needed for python3
+ignore.append('backports.os') # not needed for python3
+ignore.append('backports.shutil_get_terminal_size') # not needed for python3
+ignore.append('bkcharts') # unmaintained
+ignore.append('blas') # empty
+ignore.append('conda-build') # anaconda related
+ignore.append('conda-env') # anaconda related
+ignore.append('conda-verify') # anaconda related
+ignore.append('clyent') # not needed for linux
+ignore.append('dask-core') # already provided by python-dask
+ignore.append('distributed') # should use python-setuptools instead
+ignore.append('get_terminal_size') # empty
+ignore.append('ipython_genutils') # will be removed
+ignore.append('libcurl') # already provided by curl
+ignore.append('libgfortran-ng') # already provided by gcc-libs
+ignore.append('liblief') # already provided by lief
+ignore.append('libstdcxx-ng') # already provided by gcc-libs
+ignore.append('navigator-updater') # anaconda related
+ignore.append('simplegeneric') # already provided by ipython
+ignore.append('singledispatch') # not needed for python3
+ignore.append('sphinxcontrib') # empty
+ignore.append('unicodecsv') # not needed for python3
 
-# AUR
-ignore.append('astropy')
-ignore.append('backcall')
-ignore.append('bitarray')
-ignore.append('bkcharts')
-ignore.append('bokeh')
+# In AUR but not in arch4edu yet
 ignore.append('cytoolz')
 ignore.append('dask')
-ignore.append('fastcache')
 ignore.append('heapdict')
-ignore.append('locket')
-ignore.append('multipledispatch')
 ignore.append('partd')
-ignore.append('pep8')
-ignore.append('pycrypto')
-ignore.append('pytest-arraydiff')
-ignore.append('pytest-astropy')
-ignore.append('pytest-doctestplus')
-ignore.append('pytest-openfiles')
-ignore.append('pytest-remotedata')
-ignore.append('simplegeneric')
-ignore.append('sortedcollections')
 ignore.append('toolz')
-ignore.append('wurlitzer')
 ignore.append('zict')
-
-run_cmd(['sudo', 'pacman', '-Sy'], silent=True)
 
 def check(pkgname):
     try:
@@ -86,14 +85,12 @@ def check(pkgname):
     except:
         output = ''
     output = output.split('\n')
-    if pkgname in output:
-        return True
-    output = run_cmd(['yay', '-Ssq', pkgname], silent=True)
-    output = output.split('\n')
     return pkgname in output
 
+run_cmd(['sudo', 'pacman', '-Sy'])
 lines = run_cmd(['ls', '/opt/anaconda/conda-meta'], silent=True).split('\n')
 output = []
+failed = False
 
 for line in tqdm(lines):
     line = line.split('-')
@@ -125,7 +122,11 @@ for line in tqdm(lines):
         output.append(package)
         continue
 
-    output.append('# failed %s' % '-'.join(line))
+    print('%s failed' % '-'.join(line))
+    failed = True
+
+if failed:
+    sys.exit(1)
 
 with open('packages.txt', 'w') as f:
     f.writelines('\n'.join(output))
