fe7073b1c5c7cab3720199e1b33506b95d0e6925
Jingbei Li <i@jingbei.li>
2020-05-21 15:12:58 +0800

    fixed packages.py

--- packages.py
+++ packages.py
@@ -1,6 +1,5 @@
 #!/bin/python
 import sys
-from tqdm import tqdm
 from cmd import run_cmd
 
 force = {}
@@ -13,7 +12,6 @@ force['glib'] = 'glib2'
 force['graphite2'] = 'graphite'
 force['importlib_metadata'] = 'python-importlib-metadata'
 force['intel-openmp'] = 'intel-openmp'
-force['ipython'] = 'ipython'
 force['jbig'] = 'jbigkit'
 force['jinja2'] = 'python-jinja'
 force['jpeg'] = 'libjpeg-turbo'
@@ -21,16 +19,22 @@ force['jupyter'] = 'jupyter'
 force['jupyter_console'] = 'jupyter_console'
 force['jupyterlab'] = 'jupyterlab'
 force['jupyterlab_server'] = 'jupyterlab_server'
+force['ld_impl_linux-64'] = 'binutils'
 force['libgcc-ng'] = 'gcc-libs'
+force['libspatialindex'] = 'spatialindex'
 force['libuuid'] = 'libutil-linux'
 force['lz4-c'] = 'lz4'
+force['matplotlib-base'] = 'python-matplotlib'
 force['mkl'] = 'intel-mkl'
+force['mkl_fft'] = 'python-mkl-fft'
+force['mkl_random'] = 'python-mkl-random'
 force['msgpack-python'] = 'python-msgpack'
 force['nbconvert'] = 'jupyter-nbconvert'
 force['nbformat'] = 'jupyter-nbformat'
 force['notebook'] = 'jupyter-notebook'
 force['numpy-base'] = 'python-numpy-mkl'
 force['pandoc'] = 'pandoc'
+force['path.py'] = 'python-path'
 force['py-lief'] = 'lief'
 force['pyyaml'] = 'python-yaml'
 force['pyqt'] = 'python-pyqt5'
@@ -38,19 +42,18 @@ force['qt'] = 'qt5-base'
 force['ruamel_yaml'] = 'python-ruamel-yaml'
 force['scipy'] = 'python-scipy-mkl'
 force['spyder'] = 'spyder'
+force['tbb'] = 'intel-tbb'
 force['widgetsnbextension'] = 'jupyter-widgetsnbextension'
 force['yaml'] = 'libyaml'
 
 ignore = []
 ignore.append('_ipyw_jlab_nb_ext_conf') # anaconda related
+ignore.append('_libgcc_mutex') # empty
 ignore.append('anaconda') # anaconda related
 ignore.append('anaconda-client') # anaconda related
 ignore.append('anaconda-navigator') # anaconda related
 ignore.append('anaconda-project') # anaconda related
 ignore.append('backcall') # already provided by ipython
-ignore.append('backports') # not needed for python3
-ignore.append('backports.os') # not needed for python3
-ignore.append('backports.shutil_get_terminal_size') # not needed for python3
 ignore.append('bkcharts') # unmaintained
 ignore.append('blas') # empty
 ignore.append('conda-build') # anaconda related
@@ -71,28 +74,29 @@ ignore.append('singledispatch') # not needed for python3
 ignore.append('sphinxcontrib') # empty
 ignore.append('unicodecsv') # not needed for python3
 
-# In AUR but not in arch4edu yet
-ignore.append('cytoolz')
-ignore.append('dask')
-ignore.append('heapdict')
-ignore.append('partd')
-ignore.append('toolz')
-ignore.append('zict')
+from pyalpm import Handle
+arch = 'x86_64'
+package = 'jre8-openjdk'
+dbpath = '/var/lib/pacman/'
+handle = Handle('/', dbpath)
+for i in ['core', 'extra', 'community', 'arch4edu']:
+    handle.register_syncdb(i, 0)
 
-def check(pkgname):
-    try:
-        output = run_cmd(['pacman', '-Ssq', pkgname], silent=True)
-    except:
-        output = ''
-    output = output.split('\n')
-    return pkgname in output
+def check(package):
 
-run_cmd(['sudo', 'pacman', '-Sy'])
-lines = run_cmd(['ls', '/opt/anaconda/conda-meta'], silent=True).split('\n')
+    for repo in handle.get_syncdbs():
+        result = repo.get_pkg(package)
+        if result:
+            return result
+    return None
+
+lines = run_cmd(['pacman', '-Flq', 'anaconda'], silent=True).split('\n')
+lines = [line for line in lines if line.startswith('opt/anaconda/conda-meta/') and line.endswith('.json')]
+lines = [line[len('opt/anaconda/conda-meta/'):] for line in lines]
 output = []
 failed = False
 
-for line in tqdm(lines):
+for line in lines:
     line = line.split('-')
 
     if len(line) < 2:
@@ -110,11 +114,16 @@ for line in tqdm(lines):
         output.append(force[package])
         continue
 
+    # Ignored all backports for python3
+    if pkgver[1].startswith('py') and not package.startswith('python') and package.startswith('backports'):
+        output.append('# ignored %s' % '-'.join(line))
+        continue
+
     if pkgver[1].startswith('py') and not package.startswith('python') and check('python-%s' % package):
         output.append('python-%s' % package)
         continue
 
-    if pkgver[1].startswith('py') and package.startswith('python') and check(package):
+    if pkgver[1].startswith('py') and check(package):
         output.append(package)
         continue
 
